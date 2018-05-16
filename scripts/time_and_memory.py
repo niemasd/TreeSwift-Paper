f186110f0c28ef30cbd5bc1610e1277b9c9e70e7
Niema Moshiri <niemamoshiri@gmail.com>
2018-05-15 20:57:31 -0700

    Added Bio.Phylo

--- scripts/time_and_memory.py
+++ scripts/time_and_memory.py
@@ -5,19 +5,25 @@ def memory():
     return Process(getpid()).memory_info().rss
 
 # main code
+from io import StringIO
 from sys import argv
 from time import time
-if len(argv) != 3 or argv[2] not in {'treeswift','dendropy'}:
-    print("USAGE: %s <tree_file> <treeswift_or_dendropy>"%argv[0]); exit(1)
+if len(argv) != 3 or argv[2] not in {'treeswift','dendropy','biophylo'}:
+    print("USAGE: %s <tree_file> <treeswift_or_dendropy_or_biophylo>"%argv[0]); exit(1)
 if argv[1].lower().endswith('.gz'):
     from gzip import open as gopen
     treestr = gopen(argv[1]).read().decode().strip()
 else:
     treestr = open(argv[1]).read().strip()
+treeio = StringIO(treestr) # for Bio.Phylo
 m_start = memory(); t_start = time()
 if argv[2][0] == 'd':
     from dendropy import Tree
     for node in Tree.get(data=treestr, schema='newick').preorder_node_iter():
+        pass
+elif argv[2][0] == 'b':
+    from Bio import Phylo
+    for node in Phylo.read(treeio, 'newick').find_clades(order='preorder'):
         pass
 else:
     from treeswift import read_tree_newick
