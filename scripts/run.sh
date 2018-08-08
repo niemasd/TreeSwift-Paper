e35e5c8789544bd695929dfc8e63064bf52705c0
Niema Moshiri <niemamoshiri@gmail.com>
2018-08-08 12:25:04 -0700

    Updated scripts

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,5 +4,5 @@ m=$1
 n=$2
 t=$3
 echo -n "["
-for r in $(seq -w 1 $IT) ; do ./time.py "../data/tree_n$n.tre.gz" $t $m ; done | numlist -csv | tr -d '\n'
+for r in $(seq -w 1 $IT) ; do ./time.py "../data/tree_n$n.tre.gz" $t $m 2>/dev/null ; done | numlist -csv | tr -d '\n'
 echo "]"
