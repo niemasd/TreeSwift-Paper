9972b8d0f2103e23d6034b8a41c15a23eb1fc9d6
Niema Moshiri <niemamoshiri@gmail.com>
2018-08-08 15:02:09 -0700

    Updated figures

--- scripts/figures.py
+++ scripts/figures.py
@@ -18,22 +18,39 @@ from matplotlib.patches import Patch
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
+sns.set_context("paper", rc={"font.size":12,"axes.titlesize":16,"axes.labelsize":14,"legend.fontsize":10,"xtick.labelsize":10,"ytick.labelsize":10})
 sns.set_style("ticks")
 rcParams['font.family'] = 'serif'
 T = ['DendroPy','Bio.Phylo','ETE Toolkit','TreeSwift']
 name_to_key = {'DendroPy':'dendropy', 'Bio.Phylo':'biophylo', 'TreeSwift':'treeswift', 'ETE Toolkit':'ete3'}
 pal = {'TreeSwift':'#20641c', 'Bio.Phylo':'#6caed1', 'DendroPy':'#2286ca', 'ETE Toolkit':'#bfe49e'}
 linestyle = {'TreeSwift':'-', 'DendroPy':':', 'Bio.Phylo':'--', 'ETE Toolkit':'-.'}
-task = {
-    'distance_matrix': "Distance Matrix",
-    'inorder': "In-Order",
-    'levelorder': "Level-Order",
-    'mrca': "MRCA",
-    'postorder': "Post-Order",
-    'preorder': "Pre-Order",
-    'rootdistorder': "Root-Distance-Order",
-    "total_branch_length": "Total Length"
+meta = {
+    'name': {
+        'distance_matrix': "Distance Matrix",
+        'inorder': "In-Order Traversal",
+        'ladderize': "Ladderize",
+        'levelorder': "Level-Order Traversal",
+        'load_tree': "Load Tree",
+        'memory': "Memory Usage",
+        'mrca': "Most Recent Common Ancestor",
+        'postorder': "Post-Order Traversal",
+        'preorder': "Pre-Order Traversal",
+        'rootdistorder': "Root-Distance-Order Traversal",
+        'total_branch_length': "Total Length"
+    },
+    'ymax': {
+        'distance_matrix': 0.65,
+        'ladderize': 15,
+        'levelorder': 3.5,
+        'load_tree': 70,
+        'memory': 1200,
+        'mrca': 2.8,
+        'postorder': 4,
+        'preorder': 4,
+    }
 }
+meta['ymin'] = {m:0 for m in meta['name']}
 N = None
 for m in data:
     if N is None or len(data[m]) > len(N):
@@ -59,18 +76,17 @@ for m in sorted(data.keys()):
                 y = y/float(MEMORY_DEN)
             sns.pointplot(x=x, y=y, color=pal[t], linestyles=linestyle[t])
             used.append(t)
-    plt.ylim(ymin=0)
+    #plt.yscale('log')
+    if m in meta['ymin']:
+        plt.ylim(ymin=meta['ymin'][m])
+    if m in meta['ymax']:
+        plt.ylim(ymax=meta['ymax'][m])
+    plt.title(meta['name'][m])
+    plt.xlabel("Number of Leaves")
     if m == 'memory':
-        sns.plt.title("Memory Usage vs. Number of Leaves",fontsize=18,y=1.02)
-    elif m == 'rootdistorder':
-        sns.plt.title("Execution Time vs. Number of Leaves (%s)"%task[m],fontsize=14,y=1.02)
+        plt.ylabel("Memory Usage (MB)")
     else:
-        sns.plt.title("Execution Time vs. Number of Leaves (%s)"%task[m],fontsize=16,y=1.02)
-    sns.plt.xlabel("Number of Leaves")
-    if m == 'memory':
-        sns.plt.ylabel("Memory Usage (MB)")
-    else:
-        sns.plt.ylabel("Execution Time (seconds)")
+        plt.ylabel("Execution Time (seconds)")
     handles = [Patch(color=pal[t],label=t) for t in used]
     legend = plt.legend(handles=handles,bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0., frameon=True)
     plt.show()
